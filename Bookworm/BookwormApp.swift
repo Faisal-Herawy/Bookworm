61d93a17b50c844a4f1288d5f47a010107854003
Faisal-Herawy <faisalherawy@icloud.com>
2024-01-08 20:05:34 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Faisal Herawy on 26/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
